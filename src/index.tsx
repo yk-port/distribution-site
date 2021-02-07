faab4db71d9eb22abb5ead39f18dce95ad630da4
Tatsuya <yokokura0413@gmail.com>
2021-02-08 05:33:09 +0900

    create-top-page

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import TopPage from "./pages/TopPage";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TopPage />
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/pages/TopPage.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const TopPage: FC = () => {
+  return <h2>TopPageコンポーネント</h2>;
+};
+
+export default TopPage;
