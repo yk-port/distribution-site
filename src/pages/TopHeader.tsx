18159dd5aee5fe70e765cd7d388ff204bf7b291c
Tatsuya <yokokura0413@gmail.com>
2021-02-08 05:37:12 +0900

    build material-ui/core

---
+++ src/pages/TopHeader.tsx
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar } from "@material-ui/core";
+import React, { FC } from "react";
+
+const TopHeader: FC = () => {
+  return (
+    <AppBar>
+      <Toolbar></Toolbar>
+    </AppBar>
+  );
+};
+
+export default TopHeader;
