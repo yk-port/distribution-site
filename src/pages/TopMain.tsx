ad7c53ef0af9d037e88d4cd83ca11283736d89d3
Tatsuya <yokokura0413@gmail.com>
2021-02-08 09:40:05 +0900

    create search-function

--- src/pages/TopMain.tsx
+++ src/pages/TopMain.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import { useHistory } from "react-router-dom";
 import {
   createStyles,
   IconButton,
@@ -31,16 +32,27 @@ const useStyle = makeStyles(() =>
 
 const TopMain: FC = () => {
   const classes = useStyle();
+  const [keyword, setKeyword] = useState("");
+  const history = useHistory();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    history.push(`/search/${keyword}`);
+  };
 
   return (
     <div className={classes.background}>
-      <Paper className={classes.paper}>
+      <Paper className={classes.paper} component="form" onSubmit={handleSubmit}>
         <IconButton type="submit">
           <SearchIcon />
         </IconButton>
         <InputBase
           className={classes.inputbase}
           placeholder="検索する文字を入力して下さい"
+          onChange={handleChange}
         />
       </Paper>
     </div>
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,13 +1,21 @@
 import React, { FC } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import TopHeader from "./TopHeader";
 import TopMain from "./TopMain";
 
 const TopPage: FC = () => {
   return (
-    <>
-      <TopHeader />
-      <TopMain />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <TopHeader />
+          <TopMain />
+        </Route>
+        <Route exact path="/search/:keyword">
+          検索結果
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
